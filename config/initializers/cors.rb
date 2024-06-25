77f665df9433e6058a7ee7632edaa67734ceeb7e
smakthe <scmakra99@gmail.com>
2024-06-25 11:34:52 +0530

    Updated frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://task-manager-steel-psi.vercel.app/'
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
